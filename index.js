2016c865f82dc7d2ded72efa9e205cc5f6be004f
xWOLXz <kvin.vera@gmail.com>
2025-08-02 21:55:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
-app.use('/', require('./routes/items'));
+const itemsRoutes = require('./routes/items');
+app.use('/api', itemsRoutes);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
